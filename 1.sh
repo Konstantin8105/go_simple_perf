9789b027ba71fb1bab337463254c10a3b875ff82
Konstantin8105 <konstantin8105@gmail.com>
2018-08-29 15:26:30 +0300

    update

--- 1.sh
+++ 1.sh
@@ -1,8 +1,8 @@
 echo "========================================"
 sleep .5
 echo "= Searching #01 ========================"
-echo "= C ++   ==============================="
-gcc -o  main-gcc main.c -std=c99 -lm
+echo "= C      ==============================="
+gcc -o  main-gcc main.c -O3 -std=c99 -lm
 time ./main-gcc 100000000
 echo "= GOLANG ==============================="
 sleep 1
@@ -11,8 +11,8 @@ time ./main 100000000
 echo "========================================"
 sleep .5
 echo "= Searching #02 ========================"
-echo "= C ++   ==============================="
-gcc -o  main-gcc main.c -std=c99 -lm
+echo "= C      ==============================="
+gcc -o  main-gcc main.c -O3 -std=c99 -lm
 time ./main-gcc 150000000
 echo "= GOLANG ==============================="
 sleep 1
@@ -21,8 +21,8 @@ time ./main 150000000
 echo "========================================"
 sleep .5
 echo "= Searching #03 ========================"
-echo "= C ++   ==============================="
-gcc -o  main-gcc main.c -std=c99 -lm
+echo "= C      ==============================="
+gcc -o  main-gcc main.c -O3 -std=c99 -lm
 time ./main-gcc 1500000
 echo "= GOLANG ==============================="
 sleep 1
@@ -31,8 +31,8 @@ time ./main 1500000
 echo "========================================"
 sleep .5
 echo "= Searching #04 ========================"
-echo "= C ++   ==============================="
-gcc -o  main-gcc2 main2.c -std=c99 -lm
+echo "= C      ==============================="
+gcc -o  main-gcc2 main2.c -O3 -std=c99 -lm
 time ./main-gcc2 100000000
 echo "= GOLANG ==============================="
 sleep 1
@@ -41,8 +41,8 @@ time ./main2 100000000
 echo "========================================"
 sleep .5
 echo "= Searching #05 ========================"
-echo "= C ++   ==============================="
-gcc -o  main-gcc3 main3.c -std=c99 -lm
+echo "= C      ==============================="
+gcc -o  main-gcc3 main3.c -O3 -std=c99 -lm
 time ./main-gcc3 100000000
 echo "= GOLANG ==============================="
 sleep 1
